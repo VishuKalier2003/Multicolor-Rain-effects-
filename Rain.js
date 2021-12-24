36b9c0b9187fe72a8f975674b400234576d04a25
Vishu Kalier <95927774+VishuKalier2003@users.noreply.github.com>
2021-12-24 20:20:12 +0530

    A small code for a great visual effect
    The code provides different types of rains which are of different velocities with respect to one another such that it gives a drizzling prespective.

---
+++ Rain.js
@@ -0,0 +1,19 @@
+function rain(){
+    let amount = 250;
+    let body = document.querySelector("body");
+    let i = 0;
+    while (i < amount){
+        let drop = document.createElement('i');
+        let size = Math.random() * 5;
+        let posX = Math.floor(Math.random() * window.innerWidth);
+        let delay = Math.random() * -20;
+        let duration = Math.random() * -20;
+        drop.style.width = size + 'px';
+        drop.style.left = posX + 'px';
+        drop.style.animationDelay = delay + 's';
+        drop.style.animationDuration = 20 + duration + 's';
+        body.appendChild(drop);
+        i++;
+    }
+}
+rain();
